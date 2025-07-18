ce6772f373aed0569830c10b348fd8910161820c
Dhruvmishra19 <dhruv.mishra19@gmail.com>
2025-07-18 23:08:39 +0530

    Update locustfile.py

--- locustfile.py
+++ locustfile.py
@@ -1,14 +1,16 @@
 from locust import HttpUser, task, between
 
 class CloudUser(HttpUser):
-    host = "https://flask-api-<REGION>-<YOUR_PROJECT>.run.app"
-    wait_time = between(1,3)
+    # The host will be specified in the Locust web UI when you start a new test.
+    # The URL will be the one provided by Cloud Run for the 'flask-api' service.
+    wait_time = between(1, 3)
 
     def on_start(self):
+        """This method is called for each simulated user when they start."""
         with self.client.post("/api/login",
                               json={"username":"user","password":"pass"},
                               catch_response=True, name="Login") as resp:
-            if resp.status_code!=200:
+            if resp.status_code != 200:
                 resp.failure(f"Login failed: {resp.status_code}")
 
     @task(3)
